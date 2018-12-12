9ff357a4681d741759687a3140a1d3c69bc84a74
Xmader <xmader@outlook.com>
2018-12-12 16:39:22 +0800

    更新页脚

--- App.vue
+++ App.vue
@@ -1,17 +1,22 @@
 <template>
     <main>
         <top-menu></top-menu>
+
         <router-view></router-view>
+
+        <page-footer></page-footer>
     </main>
 </template>
 
 <script>
 
 import TopMenu from "./components/TopMenu.vue"
+import PageFooter from "./components/PageFooter.vue"
 
 export default {
     components: {
         TopMenu,
+        PageFooter
     },
     data() {
         return ({
---
+++ components/PageFooter.vue
@@ -0,0 +1,63 @@
+
+<template>
+    <div class="aw-footer-wrap">
+        <div class="aw-footer">
+            <template v-if="footerType == 'Footer'">
+                Copyright © {{ year }} {{ footer.author }}. All Rights Reserved.
+
+                <span class="hidden-xs">
+                    Powered By
+                    <a
+                        href="https://github.com/ZettaForum/core"
+                        rel="nofollow noreferrer noopener"
+                        target="_blank"
+                    >
+                        ZettaForum
+                    </a>
+                </span>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    inject: ["init"],
+    data() {
+        return ({
+        })
+    },
+    computed: {
+        footer() {
+            return this.init.footer
+        },
+        footerType() {
+            const footer = this.init.footer
+
+            if (typeof footer == "string") {
+                return "string"
+            } else if (footer instanceof Element) {
+                return "Element"
+            } else {
+                return "Footer"
+            }
+        },
+        year() {
+            if (!this.footer.year) return
+
+            const thisYear = new Date().getFullYear()
+            const foundingYear = this.footer.year
+
+            if (thisYear <= foundingYear) {
+                return `${thisYear}`
+            } else {
+                return `${foundingYear}-${thisYear}`
+            }
+        }
+    },
+}
+</script>
+
+<style>
+</style>
+
