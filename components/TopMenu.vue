92d3769ff7aafa9df9cfa969368aae5013b0d62e
Xmader <xmader@outlook.com>
2018-12-12 14:56:10 +0800

    更新顶部用户导航菜单

--- components/TopMenu.vue
+++ components/TopMenu.vue
@@ -2,7 +2,10 @@
     <div class="aw-top-menu-wrap">
         <div class="container">
 
-            <top-menu-logo :src="init.logo" :link="init.logoLink"></top-menu-logo>
+            <top-menu-logo
+                :src="init.logo"
+                :link="init.logoLink"
+            ></top-menu-logo>
 
             <top-menu-search-box></top-menu-search-box>
 
@@ -11,20 +14,39 @@
             <!-- end 导航 -->
 
             <!-- 用户栏 -->
-            <div class="aw-user-nav">
-                <!-- 登陆&注册栏 -->
-                <a
-                    class="login btn btn-normal btn-primary"
-                    href="/account/login/"
-                >登录</a>
-                <a
-                    class="register btn btn-normal btn-success"
-                    href="/account/register/"
-                >注册</a>
-                <!-- end 登陆&注册栏 -->
-            </div>
+            <top-menu-user-nav></top-menu-user-nav>
             <!-- end 用户栏 -->
+
             <!-- 发起 -->
+            <div class="aw-publish-btn">
+
+                <a
+                    id="header_publish"
+                    class="btn-primary"
+                    href="/publish/"
+                ><i class="icon icon-ask"></i>发起</a>
+                <div class="dropdown-list pull-right">
+                    <ul>
+                        <li>
+                            <form
+                                method="post"
+                                action="/publish/"
+                            >
+                                <a onclick="$(this).parents('form').submit();">问题</a>
+                            </form>
+
+                        </li>
+                        <li>
+                            <form
+                                method="post"
+                                action="/publish/article/"
+                            >
+                                <a onclick="$(this).parents('form').submit();">文章</a>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <!-- end 发起 -->
         </div>
     </div>
@@ -34,12 +56,14 @@
 import TopMenuLogo from "./TopMenuLogo.vue"
 import TopMenuSearchBox from "./TopMenuSearchBox.vue"
 import TopMenuNavbar from "./TopMenuNavbar.vue"
+import TopMenuUserNav from "./TopMenuUserNav.vue"
 
 export default {
     components: {
         TopMenuLogo,
         TopMenuSearchBox,
-        TopMenuNavbar
+        TopMenuNavbar,
+        TopMenuUserNav
     },
     inject: ["init"],
     data() {
---
+++ components/TopMenuUserNav.vue
@@ -0,0 +1,100 @@
+
+<template>
+    <div class="aw-user-nav">
+        <!-- 登陆&注册栏 -->
+        <!-- <a
+                    class="login btn btn-normal btn-primary"
+                    href="/account/login/"
+                >登录</a>
+                <a
+                    class="register btn btn-normal btn-success"
+                    href="/account/register/"
+                >注册</a> -->
+        <!-- end 登陆&注册栏 -->
+        <a
+            href="/people/xxx"
+            class="aw-user-nav-dropdown"
+        >
+            <img
+                alt=""
+                :src="'lib/img/avatar-mid-img.png'"
+            >
+
+        </a>
+        <div class="aw-dropdown dropdown-list pull-right">
+            <ul class="aw-dropdown-list">
+                <li class="hidden-xs">
+                    <a href="/account/setting/profile/"><i class="icon icon-setting"></i> 设置</a>
+                </li>
+                <li>
+                    <a href="/account/logout/"><i class="icon icon-logout"></i> 退出</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .aw-user-nav {
+        position: relative;
+        float: right;
+        margin: 10px 0 10px 10px;
+        margin: 10px 0 0 10px;
+    }
+
+    .aw-user-nav:hover .dropdown-list {
+        display: block;
+    }
+
+    .aw-user-nav img {
+        width: 32px;
+        height: 32px;
+    }
+
+    .aw-user-nav .login {
+        margin-right: 10px;
+    }
+
+    .aw-user-nav .aw-user-nav-dropdown {
+        display: block;
+        height: 42px;
+    }
+
+    .aw-user-nav .aw-user-nav-dropdown .badge {
+        position: absolute;
+        top: -8px;
+        right: -8px;
+    }
+
+    .aw-user-nav .dropdown-list {
+        top: 100%;
+        width: 122px;
+        border-top: none;
+        border-radius: 0 0 4px 4px;
+    }
+
+    .aw-user-nav .dropdown-list ul li a i {
+        vertical-align: -2px;
+        margin-right: 5px;
+        font-size: 16px;
+    }
+
+    .aw-user-nav > a img {
+        border-radius: 4px;
+    }
+
+    /* iphone竖屏幕 */
+    @media (max-width: 414px) {
+        .aw-user-nav {
+            margin-right: 10px;
+        }
+    }
+</style>
+
+
