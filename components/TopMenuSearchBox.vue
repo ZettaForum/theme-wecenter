35eacfcfe2b685ebdb5caf9d6522590cd09a367f
Xmader <xmader@outlook.com>
2018-12-12 22:57:12 +0800

    删除顶部搜索框的结果预览下拉列表

--- components/TopMenuSearchBox.vue
+++ components/TopMenuSearchBox.vue
@@ -1,10 +1,6 @@
 
 <template>
-    <div
-        class="aw-search-box hidden-xs hidden-sm"
-        @keyup.esc="hide"
-        @mouseleave="hide"
-    >
+    <div class="aw-search-box hidden-xs hidden-sm">
         <input
             class="form-control"
             type="text"
@@ -12,7 +8,6 @@
             autocomplete="off"
             v-model="text"
             @keyup.enter="search"
-            @focus="show"
         />
 
         <span
@@ -22,31 +17,6 @@
         >
             <i class="icon icon-search"></i>
         </span>
-
-        <transition name="dropdown">
-            <div
-                class="aw-dropdown"
-                v-show="showing"
-            >
-                <div class="mod-body">
-                    <p class="title">输入关键字进行搜索</p>
-                    <ul class="aw-dropdown-list collapse"></ul>
-                    <p class="search">
-                        <span>搜索:</span>
-                        <a @click="search"></a>
-                    </p>
-                </div>
-                <div class="mod-footer">
-                    <router-link
-                        to="/publish"
-                        class="pull-right btn btn-mini btn-success publish"
-                        @click="hide"
-                    >
-                        发起问题
-                    </router-link>
-                </div>
-            </div>
-        </transition>
     </div>
 </template>
 
@@ -55,7 +25,6 @@ export default {
     data() {
         return ({
             text: "",
-            showing: false
         })
     },
     methods: {
@@ -65,27 +34,9 @@ export default {
                 query: { q: this.text }
             })
         },
-        hide() {
-            this.showing = false
-        },
-        show() {
-            this.showing = true
-        }
     }
 }
 </script>
-
-<style scoped>
-    .dropdown-enter-active,
-    .dropdown-leave-active {
-        transition: opacity 0.5s;
-    }
-    .dropdown-enter,
-    .dropdown-leave-to {
-        opacity: 0;
-    }
-</style>
-
 
 <style>
     .aw-search-box {
@@ -107,43 +58,5 @@ export default {
         top: 6px;
         font-size: 18px;
         color: #999;
-    }
-
-    .aw-search-box .aw-dropdown {
-        display: block;
-        width: 355px;
-    }
-
-    .aw-search-box .aw-dropdown p {
-        height: 38px;
-        line-height: 38px;
-        margin: 0;
-        padding: 0 10px;
-        font-weight: normal;
-        border-bottom: 1px solid #f0f0f0;
-    }
-
-    .aw-search-box .aw-dropdown p.search {
-        display: none;
-    }
-
-    .aw-search-box .aw-dropdown .mod-footer {
-        height: 42px;
-        line-height: 42px;
-        background-color: #fafafa;
-    }
-
-    .aw-search-box .aw-dropdown .mod-footer .btn-success {
-        margin: 10px;
-    }
-
-    .aw-search-box .aw-dropdown .aw-dropdown-list {
-        max-height: 337px;
-        overflow-y: scroll;
-        border-bottom: 1px solid #f0f0f0;
-    }
-
-    .aw-search-box .aw-dropdown .aw-dropdown-list li.question .aw-hide-txt {
-        width: 250px;
     }
 </style>
