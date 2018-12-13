938417a51bd27e030a050ed9238650799864e3c3
Xmader <xmader@outlook.com>
2018-12-13 22:04:16 +0800

    更新首页的界面

---
+++ components/IndexNavTabs.vue
@@ -0,0 +1,124 @@
+
+<template>
+    <!-- tab切换 -->
+    <ul class="nav nav-tabs aw-nav-tabs active hidden-xs">
+        <template v-if="false">
+            <!-- 暂时隐藏 -->
+            <li>
+                <a href="/sort_type-unresponsive">
+                    等待回复
+                </a>
+            </li>
+
+            <li>
+                <a
+                    href="/sort_type-hot__day-7"
+                    id="sort_control_hot"
+                >
+                    热门
+                </a>
+            </li>
+
+            <li>
+                <a href="/is_recommend-1">
+                    推荐
+                </a>
+            </li>
+
+            <li class="active">
+                <a href="/">
+                    最新
+                </a>
+            </li>
+        </template>
+
+        <h2 class="hidden-xs">
+            <i :class="'icon icon-'+icon"></i>
+            <slot></slot>
+        </h2>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: "list"
+        },
+    },
+}
+</script>
+
+<style>
+    /* tabs切换样式 */
+
+    .aw-nav-tabs {
+        position: relative;
+        z-index: 1;
+        height: 63px;
+        margin: 0;
+        padding: 30px 20px 0;
+        border-color: #e6e6e6;
+    }
+
+    .aw-nav-tabs.active > li {
+        float: right;
+    }
+
+    .aw-nav-tabs > li {
+        margin: 0 4px 0 0;
+        font-size: 14px;
+    }
+
+    .aw-nav-tabs > li > a {
+        line-height: 30px;
+        padding: 0 13px;
+        background-color: #fff;
+        border: none;
+        cursor: pointer;
+    }
+
+    .aw-nav-tabs > li > a:hover {
+        border: none;
+        border-bottom: 2px solid #155faa;
+        background-color: #fff;
+        color: #333;
+    }
+
+    .aw-nav-tabs > li.active a {
+        line-height: 30px;
+        background-color: #fff;
+        border: none;
+        border-bottom: 2px solid #155faa;
+        font-weight: bold;
+        color: #333;
+    }
+
+    .aw-nav-tabs > li.active a:hover,
+    .aw-nav-tabs > li.active a:focus,
+    .aw-nav-tabs > li > a:focus {
+        border: none;
+        border-bottom: 2px solid #155faa;
+        background-color: #fff;
+    }
+
+    .aw-nav-tabs h2 {
+        position: absolute;
+        top: 16px;
+        left: 20px;
+        margin: 0;
+        font-size: 20px;
+    }
+
+    @media (max-width: 736px) {
+        .aw-nav-tabs {
+            padding: 30px 10px 0;
+        }
+        .aw-nav-tabs > li {
+            margin: 0;
+        }
+    }
+</style>
+
+
---
+++ components/IndexNotificationBox.vue
@@ -0,0 +1,99 @@
+<template>
+    <!-- 新消息通知 -->
+    <div
+        class="aw-mod aw-notification-box collapse"
+        id="index_notification"
+    >
+        <div class="mod-head common-head">
+            <h2>
+                <span class="pull-right"><a
+                        href="/account/setting/privacy/#notifications"
+                        class="text-color-999"
+                    ><i class="icon icon-setting"></i> 通知设置</a></span>
+                <i class="icon icon-bell"></i>新通知<em
+                    class="badge badge-important"
+                    name="notification_unread_num"
+                ></em>
+            </h2>
+        </div>
+        <div class="mod-body">
+            <ul id="notification_list"></ul>
+        </div>
+        <div class="mod-footer clearfix">
+            <a
+                href="javascript:;"
+                onclick="AWS.Message.read_notification(false, 0, false);"
+                class="pull-left btn btn-mini btn-gray"
+            >我知道了</a>
+            <a
+                href="/notifications/"
+                class="pull-right btn btn-mini btn-success"
+            >查看所有</a>
+        </div>
+    </div>
+    <!-- end 新消息通知 -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    /* 动态首页新通知提示box */
+
+    .aw-notification-box {
+        padding-bottom: 20px;
+        border-bottom: 1px solid #e6e6e6;
+    }
+
+    .aw-notification-box .icon-tips {
+        display: none;
+    }
+
+    .aw-notification-box .mod-body,
+    .aw-notification-box .mod-footer {
+        padding: 0 20px;
+    }
+
+    .aw-notification-box .mod-body ul li {
+        position: relative;
+        padding: 10px 0;
+        border-top: 1px dotted #e2e2e2;
+        line-height: 14px;
+    }
+
+    .aw-notification-box .mod-body ul li:first-child {
+        border-top: none;
+    }
+
+    .aw-notification-box .mod-body ul li span {
+        font-size: 12px;
+    }
+
+    .aw-notification-box .mod-body ul li p {
+        margin: 0;
+        line-height: 24px;
+    }
+
+    .aw-notification-box .icon-tips {
+        position: absolute;
+        right: 0;
+    }
+
+    .aw-notification-box .mod-body ul li .read {
+        display: none;
+        color: #999;
+    }
+
+    .aw-notification-box .mod-body ul li .more {
+        margin: 5px 0 0 5px;
+        font-size: 12px;
+    }
+
+    .aw-notification-box .mod-body ul li:hover .read {
+        display: block;
+    }
+</style>
+
